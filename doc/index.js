77092b2c69d01ecdc5a33863a4d3d10fdfdaf4b9
xuexb <fe.xiaowu@gmail.com>
2015-11-25 01:02:35 +0800

    测试勾子

--- doc/index.js
+++ doc/index.js
@@ -9,26 +9,20 @@ var app = new Mdjs({
     "cache_path": "../__cache/",
     "name": "apijs",
     "dir_alias": {
-        "doc": "文档yxds"
+        "doc": "文档"
     }
 });
 
 // 更新勾子
 app.express.post('/api/update', function (req, res, next) {
-    child_process.exec('pwd && git pull', {
-        cwd: app.options.root + '/doc/'
-    }, function (a, b) {
+    child_process.exec('git pull', function (a, b) {
         console.log('更新成功', b);
 
         // 清空缓存
         app.clear_cache();
 
-        console.log('clear_cache');
-
         // 重启pm2，重启是为了让配置生效
-        child_process.exec('npm run restart', {
-            cwd: app.options.root + '/doc/'
-        }, function(){
+        child_process.exec('npm run restart', function(){
             console.log('npm run restart');
         });
     });
