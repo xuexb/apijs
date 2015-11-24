5749adb8c401ffe7d279121476bc429b9c50810d
xuexb <fe.xiaowu@gmail.com>
2015-11-25 00:53:24 +0800

    优化勾子

--- doc/index.js
+++ doc/index.js
@@ -14,15 +14,15 @@ var app = new Mdjs({
 
 // 更新勾子
 app.express.post('/api/update', function (req, res, next) {
-    child_process.exec('git pull', {
-        cwd: app.options.root
-    }, function (a, b) {
+    child_process.exec('git pull', function (a, b) {
+        console.log('更新成功', b);
+
         // 清空缓存
         app.clear_cache();
 
         // 重启pm2，重启是为了让配置生效
-        child_process.exec('pm2 restart index.js', {
-            cwd: app.options.root
+        child_process.exec('npm run restart', function(){
+            console.log('npm run restart');
         });
     });
 
